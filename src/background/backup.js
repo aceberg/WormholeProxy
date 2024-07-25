f5fb9cac06b8e904b1954bd94bbeb7cbd4dc2a3e
aceberg <1502200+aceberg@users.noreply.github.com>
2024-07-26 03:22:36 +0700

    Backup - better func

--- src/background/backup.js
+++ src/background/backup.js
@@ -1,60 +1,75 @@
 let enableBackup = 0;
-let counter = 1;
+let lastBackupDate;
 
-const oneDay = 1000 * 60 * 60 * 24;
-// const oneDay = 1000 * 60; // One minute
+let proxyServers = [];
 
-setInterval(mainLoop, oneDay);
-
-function mainLoop() {
-
-  // console.log("BKP LOOP, enableBkp =", enableBackup, "counter =", counter);
+// Listen for changes in sync storage
+browser.storage.sync.onChanged.addListener(changeData => {
+  if (changeData.proxyHosts) {
+    proxyHosts = changeData.proxyHosts.newValue;
+  }
+  if (changeData.ignoreHosts) {
+    ignoreHosts = changeData.ignoreHosts.newValue;
+  }
+  if (changeData.proxyServers) {
+    proxyServers = changeData.proxyServers.newValue;
+  }
 
   browser.storage.local.get(data1 => {
     if (data1.enableBackup) {
       enableBackup = data1.enableBackup;
     };
+    if (data1.lastBackupDate) {
+      lastBackupDate = data1.lastBackupDate;
+    };
+    // console.log("LAST BKP", lastBackupDate);
 
-    if (enableBackup > 0) {
-      if (counter < enableBackup) {
-        counter = counter + 1;
-      } else {
-        counter = 1;
-        console.log("Generating backup once in "+enableBackup+" days");
-        generateBackup();
-      }
+    if (enableBackup > 0 && enoughTimePassed(lastBackupDate, enableBackup)) {
+
+      lastBackupDate = new Date();
+      browser.storage.local.set({
+        lastBackupDate: lastBackupDate
+      });
+
+      const all = {hosts: proxyHosts, servers: proxyServers, ignore: ignoreHosts};
+      generateBackup(all);
     }
   });
-};
+});
 
-function generateBackup() {
-    browser.storage.sync.get(data => {
-      if (data.proxyHosts) {
-        proxyHosts = data.proxyHosts;
-      };
-      if (data.proxyServers) {
-        proxyServers = data.proxyServers;
-      };
-      if (data.ignoreHosts) {
-        ignoreHosts = data.ignoreHosts;
-      };
-      
-      const all = {hosts: proxyHosts, servers: proxyServers, ignore: ignoreHosts};
+function enoughTimePassed(lastDate, interval) {
+  if (lastDate == undefined) {
+    return true;
+  } else {
+    const days = (new Date() - lastDate)/ (1000 * 60 * 60 * 24);
+    // console.log("DAYS", days);
     
-      const blob = new Blob([JSON.stringify(all)], { type: "application/json" });
-      const jsonObjectUrl = URL.createObjectURL(blob);
+    if (days > interval) {
+      return true;
+    }
+  }
 
-      downloadAll(jsonObjectUrl);        
-    });
+  return false;
+}
+
+function generateBackup(all) {
+
+  const blob = new Blob([JSON.stringify(all)], { type: "application/json" });
+  const jsonObjectUrl = URL.createObjectURL(blob);
+
+  downloadAll(jsonObjectUrl);        
 };
 
 function downloadAll(link) {
 
   const today = new Date().toLocaleDateString('en-CA');
+  const fname = "WormholeProxy-all-"+today+".json";
+
+  console.log("Downloading backup file: ", fname);
 
   browser.downloads.download({
     url: link,
-    filename: "WormholeProxy-all-"+today+".json",
+    filename: fname,
     conflictAction: "uniquify",
   });
 };
